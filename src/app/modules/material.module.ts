41c65682f3d02772e9ad9bdf112c3620e89ed917
zuratama <tuandt66742@gmail.com>
2020-08-19 22:25:47 +0700

    Construct project base template

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 const MODULES = [
+  LayoutModule,
   MatToolbarModule,
   MatSidenavModule,
   MatButtonModule,
   MatIconModule,
-  MatCardModule
+  MatCardModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule
 ];
 
 @NgModule({
-  declarations: MODULES,
-  imports: [CommonModule],
+  declarations: [],
+  imports: MODULES,
   exports: MODULES
 })
 export class MaterialModule {}
